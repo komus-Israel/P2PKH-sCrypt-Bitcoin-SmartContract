6a6a90c33a05bd3f86e349aa42b68c17658fe620
komus-Israel <komolehinIsrael@gmail.com>
2022-03-13 20:49:46 +0100

    initialized a private and public key

--- test/P2PKH.test.js
+++ test/P2PKH.test.js
@@ -2,4 +2,8 @@ require("chai")
     .use(require("chai-as-promised"))
     .should()
 
-const {  contract, bsv } = require("scryptlib")
+const {  bsv, buildContractClass, Ripemd160, Sig, PubKey, signTx, toHex } = require("scryptlib")
+
+const privateKey = bsv.PrivateKey.fromRandom("testnet")
+const publicKey = privateKey.publicKey
+
